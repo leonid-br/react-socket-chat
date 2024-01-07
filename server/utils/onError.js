690613c5ee24f8d9402c8d1b8077ace5eabea809
Leonid <l.bryzgalov@kaizen.run>
2024-01-07 19:14:12 +0400

    server and socket settings

---
+++ server/utils/onError.js
@@ -0,0 +1,10 @@
+export  function onError(err, req, res, next) {
+    // если имеется объект ответа
+    if (res) {
+      // статус ошибки
+      const status = err.status || err.statusCode || 500
+      // сообщение об ошибке
+      const message = err.message || 'Something went wrong. Try again later'
+      res.status(status).json({ message })
+    }
+  }
